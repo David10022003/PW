169ef96fe50326b489123b571f9876135a1bc1e0
David10022003 <86742212+David10022003@users.noreply.github.com>
2023-03-30 10:22:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/*Titulo*/
-const tabs = document.querySelectorAll('.tabs input');
-const content = document.querySelector('.content');
-const container = document.querySelectorAll('.container slider');
-let currentIndex = 0;
-let prevIndex = 0;
-
-function changeSlide(index) {
-  // Cambiar el índice actual y anterior
-  prevIndex = currentIndex;
-  currentIndex = index;
-
-  // Actualizar la selección de pestañas y botones
-  tabs[prevIndex].checked = false;
-  tabs[currentIndex].checked = true;
-
-  // Animar el desplazamiento del carrusel
-  container.changeSlide;
-}
-
-setInterval(() => {
-  // Cambiar al siguiente índice
-  changeSlide((currentIndex + 1) % tabs.length);
-}, 5000);
 /*Tarjetas*/
 
   function toggle(clase){
@@ -29,6 +5,4 @@ setInterval(() => {
     blur.classList.toggle('active')
     var popup = document.getElementById(clase);
     popup.classList.toggle('active')
-
-
 }
