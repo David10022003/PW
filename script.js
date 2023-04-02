2a8edd93419af3c584b89a0319b8c39dcab49f69
David10022003 <86742212+David10022003@users.noreply.github.com>
2023-04-02 18:49:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*Parallax*/
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth = e.pageX*speed)/100
+        const y = (window.innerHeight = e.pageY*speed)/100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+
+    });
+}
+
 /*Tarjetas*/
 
   function toggle(clase){
@@ -5,4 +19,6 @@
     blur.classList.toggle('active')
     var popup = document.getElementById(clase);
     popup.classList.toggle('active')
+
+
 }
