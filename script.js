e79758feab402128ede8ef0af8b5f1fb62836180
David10022003 <86742212+David10022003@users.noreply.github.com>
2023-05-13 19:35:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,53 @@ function parallax(e){
 
 
 }
+
+/* Entrega actividad*/
+function abrir(clase){
+    const ventana = document.getElementById(clase)
+    const ventanaInterna = ventana.querySelector('.ventana-interna-inf')
+    const carousel = ventanaInterna.querySelector('.carousel');
+    const images = carousel.querySelectorAll('img');
+    const prevBtn = ventana.querySelector('.prev-btn');
+    const nextBtn = ventana.querySelector('.next-btn');
+    let currentImageIndex = 0;
+
+    //Eliminar clase active
+    images.forEach((image) =>{
+        image.classList.remove('active');
+    });
+
+    // Mostrar la primera imagen al cargar la página
+    images[currentImageIndex].classList.add('active');
+
+    // Función para cambiar de imagen
+    function changeImage(index) {
+    // Ocultar imagen actual
+    images[currentImageIndex].classList.remove('active');
+    // Actualizar índice de imagen actual
+    currentImageIndex = (index + images.length) % images.length;
+    // Mostrar nueva imagen
+    images[currentImageIndex].classList.add('active');
+    }
+
+    // Manejadores de eventos para botones
+    prevBtn.addEventListener('click', () => {
+    changeImage(currentImageIndex - 1);
+    });
+
+    nextBtn.addEventListener('click', () => {
+    changeImage(currentImageIndex + 1);
+    });
+    var blur = document.getElementById('infografias');
+    blur.classList.toggle('active')
+    var popup = document.getElementById(clase);
+    popup.classList.toggle('active')
+}
+
+/*Ampliar imagen*/
+
+function ampliar(ventana){
+
+    var carrusel = ventana.querySelector('.active')
+    carrusel.classList.toggle('ampliada');
+}
